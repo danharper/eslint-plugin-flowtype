9d0a033aa7eb5e7b7a58a5358022c6e4bf1049d7
Dan Harper <intouch@danharper.me>
2016-09-06 22:20:01 +0100

    dangle, multiline, always & only opts too

--- src/rules/objectTypeDangle.js
+++ src/rules/objectTypeDangle.js
@@ -1,30 +1,57 @@
+import _ from 'lodash'
+
 export default (context) => {
-  const always = (context.options[0] || 'always') === 'always';
+  const option = (context.options[0] || 'always');
   const sourceCode = context.getSourceCode();
 
   return {
-    ObjectTypeProperty (node) {
-      const lastToken = sourceCode.getLastToken(node);
-      const isDangling = [';', ','].indexOf(lastToken.value) > -1;
+    ObjectTypeAnnotation (node) {
+      const lastProperty = _.last(node.properties);
 
-      if (!isDangling && always) {
+      const lastTokenOfProperty = sourceCode.getLastToken(lastProperty);
+      const lastTokenOfObject = sourceCode.getLastToken(node);
+
+      const isMultiLine = lastTokenOfProperty.loc.start.line !== lastTokenOfObject.loc.start.line;
+      const isDangling = [';', ','].indexOf(lastTokenOfProperty.value) > -1;
+
+      const reportDangle = () => {
         context.report({
           fix: (fixer) => {
-            return fixer.insertTextAfter(node, ',');
-          },
-          message: 'Must dangle',
-          node
-        });
-      }
-
-      if (isDangling && !always) {
-        context.report({
-          fix: (fixer) => {
-            return fixer.replaceText(lastToken, '');
+            return fixer.replaceText(lastTokenOfProperty, '');
           },
           message: 'Must not dangle',
-          node
+          node: lastProperty
         });
+      };
+
+      const reportNoDangle = () => {
+        context.report({
+          fix: (fixer) => {
+            return fixer.insertTextAfter(lastProperty, ',');
+          },
+          message: 'Must dangle',
+          node: lastProperty
+        });
+      };
+
+      if (option === 'always' && !isDangling) {
+        reportNoDangle();
+      }
+
+      if (option === 'never' && isDangling) {
+        reportDangle();
+      }
+
+      if (option === 'always-multiline' && !isDangling && isMultiLine) {
+        reportNoDangle();
+      }
+
+      if (option === 'always-multiline' && isDangling && !isMultiLine) {
+        reportDangle();
+      }
+
+      if (option === 'only-multiline' && isDangling && !isMultiLine) {
+        reportDangle();
       }
     }
   };
--- tests/rules/assertions/objectTypeDangle.js
+++ tests/rules/assertions/objectTypeDangle.js
@@ -16,7 +16,36 @@ export default {
       errors: [{message: 'Must not dangle'}],
       options: ['never'],
       output: 'type X = { foo: string }'
-    }
+    },
+    {
+      code: 'type X = {\nfoo: string\n}',
+      errors: [{message: 'Must dangle'}],
+      output: 'type X = {\nfoo: string,\n}'
+    },
+    {
+      code: 'type X = {\nfoo: string,\n}',
+      errors: [{message: 'Must not dangle'}],
+      options: ['never'],
+      output: 'type X = {\nfoo: string\n}'
+    },
+    {
+      code: 'type X = { foo: string, }',
+      errors: [{message: 'Must not dangle'}],
+      options: ['always-multiline'],
+      output: 'type X = { foo: string }'
+    },
+    {
+      code: 'type X = {\nfoo: string\n}',
+      errors: [{message: 'Must dangle'}],
+      options: ['always-multiline'],
+      output: 'type X = {\nfoo: string,\n}'
+    },
+    {
+      code: 'type X = { foo: string; }',
+      errors: [{message: 'Must not dangle'}],
+      options: ['only-multiline'],
+      output: 'type X = { foo: string }'
+    },
   ],
   valid: [
     {
@@ -28,6 +57,41 @@ export default {
     {
       code: 'type X = { foo: string }',
       options: ['never']
-    }
+    },
+    {
+      code: 'type X = {\nfoo: string,\n}'
+    },
+    {
+      code: 'type X = {\nfoo: string\n}',
+      options: ['never'],
+    },
+    {
+      code: 'type X = { foo: string }',
+      options: ['always-multiline']
+    },
+    {
+      code: 'type X = {\nfoo: string,\n}',
+      options: ['always-multiline']
+    },
+    {
+      code: 'type X = {\nfoo: string;\n}',
+      options: ['always-multiline']
+    },
+    {
+      code: 'type X = { foo: string }',
+      options: ['only-multiline']
+    },
+    {
+      code: 'type X = {\nfoo: string\n}',
+      options: ['only-multiline']
+    },
+    {
+      code: 'type X = {\nfoo: string,\n}',
+      options: ['only-multiline']
+    },
+    {
+      code: 'type X = {\nfoo: string;\n}',
+      options: ['only-multiline']
+    },
   ]
 };
